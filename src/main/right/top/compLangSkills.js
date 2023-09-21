0a36ea93c9d6a8cce8e6b928cacfc9551261091b
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-09-21 13:41:23 -0500

    add: logo imgs + edit: compLangSkills content

--- src/main/right/top/compLangSkills.js
+++ src/main/right/top/compLangSkills.js
@@ -1,8 +1,12 @@
 import { Skill } from '../createNewItem';
 import HTML from './imgs/HTML.png';
 import CSS from './imgs/CSS.png';
+import JS from './imgs/JS.png';
+import CL from './imgs/commandLine.png';
+import VM from './imgs/VirtualMachine.png';
+import WB from './imgs/webpack.png';
 import wordpress from './imgs/WordPress.png';
-import squarespace from './imgs/squarespace.png';
+import squarespace from './imgs/Squarespace.png';
 import './compLangSkills.css';
 
 export const skills = function () {
@@ -13,6 +17,10 @@ export const skills = function () {
 
   const html = new Skill(HTML, 'HTML');
   const css = new Skill(CSS, 'CSS');
+  const js = new Skill(JS, 'JavaScript');
+  const cl = new Skill(CL, 'Command Line');
+  const vm = new Skill(VM, 'Virtual Machine');
+  const wb = new Skill(WB, 'Webpack');
 
   const expWithTitle = document.createElement('h1');
   expWithTitle.innerText = 'Experience With:';
@@ -24,6 +32,10 @@ export const skills = function () {
     compTitle,
     html.createNew(),
     css.createNew(),
+    js.createNew(),
+    cl.createNew(),
+    vm.createNew(),
+    wb.createNew(),
     expWithTitle,
     wp.createNew(),
     sp.createNew(),
